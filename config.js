b27917de2b0dc343cb120232339972217ff22936
Saule BN <beyonce@thinkful.com>
2017-06-06 20:13:06 -0400

    env file

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||'mongodb://localhost/blogging-app';
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL ||'mongodb://localhost/blogging-app';
+exports.DATABASE_URL = 'mongodb://saule:123@ds113282.mlab.com:13282/blogging-app';
+
 exports.PORT = process.env.PORT || 8080;
